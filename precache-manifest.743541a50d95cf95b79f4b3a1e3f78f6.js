1c3a82c6f54cd896ea0ed99c00a1f6dc5ea23ca0
pat <patlehmann1@gmail.com>
2018-12-03 19:33:43 -0500

    Updates

---
+++ precache-manifest.743541a50d95cf95b79f4b3a1e3f78f6.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "6fa537a005fe2141dbe6d77eb544755d",
+    "url": "/static/media/crystals.6fa537a0.jpg"
+  },
+  {
+    "revision": "f0d0f559563590989a2c",
+    "url": "/static/css/main.1198a1cd.chunk.css"
+  },
+  {
+    "revision": "91ea072c0dc511c98d6c",
+    "url": "/static/js/1.91ea072c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c2ac02fa33e120e1c4522e6c24466d8d",
+    "url": "/static/media/wave.c2ac02fa.jpg"
+  },
+  {
+    "revision": "4fc7a89ea692e67c705398156d24f248",
+    "url": "/static/media/api-site.4fc7a89e.jpg"
+  },
+  {
+    "revision": "bba1119e4c626184ba4d85dc30c8c450",
+    "url": "/static/media/clicky-game.bba1119e.png"
+  },
+  {
+    "revision": "74c56474c90711063e4c636ab766a12e",
+    "url": "/static/media/musicSite.74c56474.jpg"
+  },
+  {
+    "revision": "f0d0f559563590989a2c",
+    "url": "/static/js/main.f0d0f559.chunk.js"
+  },
+  {
+    "revision": "cb18cccb61b72aa8b0927243916bfc59",
+    "url": "/static/media/World_Cup.cb18cccb.jpg"
+  },
+  {
+    "revision": "0a839bb30e24da82e350599dd907c461",
+    "url": "/static/media/liri_node_app.0a839bb3.jpg"
+  },
+  {
+    "revision": "9f2206fa7a998d279b60f36beb263c1f",
+    "url": "/static/media/node_mysql_store.9f2206fa.jpg"
+  },
+  {
+    "revision": "918eee9e26a4d1480089775b1b0a199a",
+    "url": "/static/media/DinnerBell.918eee9e.png"
+  },
+  {
+    "revision": "60ca2302e8a356b95ced258420871a52",
+    "url": "/static/media/newsday_headlines.60ca2302.jpg"
+  },
+  {
+    "revision": "086b507b175c171fb04071eb6bd415c0",
+    "url": "/static/media/pouches.086b507b.jpg"
+  },
+  {
+    "revision": "9a21da6ec955d0fde3f73ef650fe9af4",
+    "url": "/static/media/eat_da_burger.9a21da6e.jpg"
+  },
+  {
+    "revision": "ed3dbcc01f93b659186ac0aa7da3d264",
+    "url": "/index.html"
+  }
+];
